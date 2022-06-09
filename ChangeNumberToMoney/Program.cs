265bb659bd1d642a08d923cbed8a2498c92f5803
soulflysk <soulflysk@outlook.co.th>
2022-06-09 12:31:05 +0700

    Add project files.

---
+++ ChangeNumberToMoney/Program.cs
@@ -0,0 +1,45 @@
+﻿
+Console.Write("Please enter your money: ");
+string number = Convert.ToString(Console.ReadLine());
+
+public enum Number
+{
+    หนึ่ง,
+    สอง,
+    สาม,
+    สี่,
+    ห้า,
+    หก,
+    เจ็ด,
+    แปด,
+    เก้า,
+    สิบ
+}
+// slice int number to array
+
+char[] chars = number.ToCharArray();
+//เปลี่ยนตัวเลขเป็นอักษรโดยการแทนที่
+foreach (char c in chars)
+{
+    switch (c)
+    {
+        case '1':
+            c = "หนึ่ง";
+            break;
+        case '2':
+            c = "สอง";
+            break;
+            ...
+
+    }
+
+}
+
+
+//แทรก สิบ ร้อย พัน หมื่น แสน ล้าน ระหว่าง array โดย array ที่ 2 คือหลักสิบ เป็ฯต้น
+chars[1].pop();
+chars[1] = "สิบ";
+chars[3].pop();
+chars[3] = "พัน"ว
+    ...
+// 
