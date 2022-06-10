a695c617654b845bfcabbc0299ece6962ed61500
soulflysk <soulflysk@outlook.co.th>
2022-06-10 09:32:41 +0700

    fix program 10 jun

--- ChangeNumberToMoney/Program.cs
+++ ChangeNumberToMoney/Program.cs
@@ -2,51 +2,63 @@
 Console.Write("Please enter your money: ");
 string number = Convert.ToString(Console.ReadLine());
 
-public enum Number
-{
-    หนึ่ง,
-    สอง,
-    สาม,
-    สี่,
-    ห้า,
-    หก,
-    เจ็ด,
-    แปด,
-    เก้า,
-    สิบ
-}
 // slice int number to array
 
 char[] chars = number.ToCharArray();
+string result = "";
 //เปลี่ยนตัวเลขเป็นอักษรโดยการแทนที่
 foreach (char c in chars)
 {
     switch (c)
     {
         case '1':
-            c = "หนึ่ง";
+            result += "one";
             break;
         case '2':
-            c = "สอง";
+            result += "two";
             break;
-            ...
+        case '3':
+            result += "three";
+            break;
+        case '4':
+            result += "four";
+            break;
+        case '5':
+            result += "five";
+            break;
+        case '6':
+            result += "six";
+            break;
+        case '7':
+            result += "seven";
+            break;
+        case '8':
+            result += "eight";
+            break;
+        case '9':
+            result += "nine";
+            break;
+        case '0':
+            result += "ten";
+            break;
 
     }
-
 }
-
-
-//แทรก สิบ ร้อย พัน หมื่น แสน ล้าน ระหว่าง array โดย array ที่ 2 คือหลักสิบ เป็ฯต้น
+//มีปัญหาในการอ้างอิงคำในตัวแปร result
+//แทรก สิบ ร้อย พัน หมื่น แสน ล้าน ระหว่าง array โดย array ที่ 2 คือหลักสิบ เป็นต้น
 chars[1].pop();
 chars[1] = "สิบ";
 chars[3].pop();
-chars[3] = "พัน"ว
+chars[3] = "พัน";
     ...
 // 
-if(chars[0] = "หนึ่ง") {
+if (chars[0] = "หนึ่ง")
+{
     chars[0] = "เอ็ด";
 }
-if(chars[1] = "สอง")
+if (chars[1] = "สอง")
 {
     chars[2] = "ยี่สิบ"
 }
+
+Console.ReadLine();
